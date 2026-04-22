436cfa99774abeafbef3101db81aba249eb66eb9
kaua kirk <103045211+kauakirk@users.noreply.github.com>
2026-04-22 03:42:59 +0000

    estudando JS

--- index.js
+++ index.js
@@ -27,3 +27,39 @@ console.log(`O valor de a é ${a} e o valor de b é ${b}.`)
 const numeros2 = [1, 2, 3, 4]
 
 const numerosDif = [...numeros2, 5, 6, 7]
+
+const usuario = {
+    primeiroNome: "João",
+    sobreNome: "Silva",
+    idade: 25
+};
+
+const {primeiroNome, sobreNome, idade} = usuario;
+
+const {novoA, novoB, ...rest} = {a: 10, b: 20, c: 30, d: 40}
+
+
+const numeros = [1, 2, 3, 4, 5]
+
+const  [primeiroNumero, segundoNumero, segundoNumero2, terceiroNumero, quartoNumero] = numeros
+console.log(primeiroNumero, segundoNumero, segundoNumero2, terceiroNumero, quartoNumero)
+
+
+let c = 5
+let f = 20
+[c, f] = [f, c]
+
+
+const series = ["Breaking Bad", "Game of Thrones", "Stranger Things"]
+const series2 = [...series, "The Crown", "The Mandalorian"]
+
+
+const viking = {
+    familia: "Ragnarsson",
+    idade: 35,
+    tamanho: "grande"
+};
+
+const {familia, novaIdade, tamanho} = viking
+
+console.log(familia, novaIdade, tamanho)
